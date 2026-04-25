7572e07fe8e27c7f3cb07a46751c88e5f9c8a063
jasmin-f <jasminfaessler.ch#gmail.com>
2026-04-25 14:39:01 +0200

    home manager test version wiiieder unerlaubt gendert..

--- flake.nix
+++ flake.nix
@@ -1,11 +1,13 @@
 {
-  description = "Home Manager configuration of jf";
+  description = "Home Manager configuration of jasmin";
 
   inputs = {
     # Specify the source of Home Manager and Nixpkgs.
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    home-manager = {
-      url = "github:nix-community/home-manager";
+#    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+ 
+	nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+ home-manager = {
+      url = "github:nix-community/home-manager/release-23.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
@@ -13,20 +15,28 @@
   outputs =
     { nixpkgs, home-manager, ... }:
     let
-      system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
+    	lib = nixpkgs.lib;
+     	 system = "x86_64-linux";
+     	 #pkgs = nixpkgs.legacyPackages.${system};
+    	pkgs = import nixpkgs { inherit system; };
     in
     {
-      #homeConfigurations."jf" = home-manager.lib.homeManagerConfiguration {
-      homeConfigurations."jasmin" = home-manager.lib.homeManagerConfiguration {
+     	 #homeConfigurations."jf" = home-manager.lib.homeManagerConfiguration {
+	    #  homeConfigurations."jasmin" = home-manager.lib.homeManagerConfiguration {
 
-        inherit pkgs;
+    	 #   inherit pkgs;
         # Specify your home configuration modules here, for example,
         # the path to your home.nix.
-        modules = [ ./home.nix ];
+     	 #  modules = [ ./home.nix ];
 
         # Optionally use extraSpecialArgs
         # to pass through arguments to home.nix
-      };
+     	# };
+     homeConfigurations = {
+     	jasmin = home-manager.lib.homeManagerConfiguration {
+		inherit pkgs;
+		modules = [ ./home.nix ];
+	};
+     };
     };
 }
---
+++ home.nix
@@ -0,0 +1,57 @@
+{ lib, pkgs, ... }:
+
+{
+  # Home Manager needs a bit of information about you and the paths it should
+  # manage.
+  home.username = "jasmin";
+  home.homeDirectory = "/home/jasmin";
+
+  # This value determines the Home Manager release that your configuration is
+  # compatible with. This helps avoid breakage when a new Home Manager release
+  # introduces backwards incompatible changes.
+  #
+  # You should not change this value, even if you update Home Manager. If you do
+  # want to update the value, then make sure to first check the Home Manager
+  # release notes.
+home.stateVersion = "25.05"; # Please read the comment before changing.
+#	home.stateVersion = "25.11"; 
+
+
+
+  # The home.packages option allows you to install Nix packages into your
+  # environment.
+  home.packages = with pkgs; [
+    
+    hello
+
+    # man-pages man-pages-posix # manpages, info: https://wiki.nixos.org/wiki/Man_pages, test with "man 3p putenv"
+
+#    podman
+    # pkgs.hello
+
+    # # It is sometimes useful to fine-tune packages, for example, by applying
+    # # overrides. You can do that directly here, just don't forget the
+    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
+    # # fonts?
+    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
+
+    # # You can also create simple shell scripts directly inside your
+    # # configuration. For example, this adds a command 'my-hello' to your
+    # # environment:
+    # (pkgs.writeShellScriptBin "my-hello" ''
+    #   echo "Hello, ${config.home.username}!"
+    # '')
+  ];
+
+  # Home Manager is pretty good at managing dotfiles. The primary way to manage
+
+#   programs.home-manager.enable = true;
+
+#  xdg.mimeApps.defaultApplications = {
+ #   "text/html" = ["firefox.desktop"];
+  #  "text/xml" = ["firefox.desktop"];
+#    "x-scheme-handler/http" = ["firefox.desktop"];
+ #   "x-scheme-handler/https" = ["firefox.desktop"];
+ # };
+
+}
